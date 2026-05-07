30452baa52cca2c7346adb625d30f8c043d41131
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-07 17:32:51 +0900

    Add JSON user storage functions

--- storage.py
+++ storage.py
@@ -1,5 +1,26 @@
+import json
 
-storage = {"andrew": "barcelon"}
+
+def load_cli():
+
+    try:
+        with open("users.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+
+
+    except FileNotFoundError:
+        return {}
+
+    except json.JSONDecodeError:
+        print("error, the file type and is not json format!")
+        return None
+
+
+
+def save_cli(users):
+    with open("users.json", "w", encoding="utf-8") as file:
+        json.dump(users, file, indent=4)
+
 
 
 
