1e8fa573fb4a4ad31ad58e37f19cebb84c9a7eff
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-10 22:09:52 +0900

    validation refactor for square_box

--- ui.py
+++ ui.py
@@ -3,6 +3,9 @@
 def square_box(title, options):
     width = 34
 
+    def print_line(text):
+        print("| " + text.ljust(width - 2) + " |")
+
     print("+" + "-" * width + "+")
     print("|" + title.center(width) + "|")
     print("+" + "-" * width + "+")
@@ -12,3 +15,28 @@ def square_box(title, options):
     for option in options:
         print("| " + option.ljust(width - 2) + " |")
         print("+" + "-" * width + "+")
+
+
+    if not options:
+        print("data not found")
+
+
+    elif isinstance(title, dict):
+        for username, password in options.items():
+            print_line("Username: " + username)
+            print_line("Password: " + password)
+            print_line("-" * (width - 2))
+
+    else:
+        for content in options:
+            print_line(content)
+
+
+
+
+    print("+" + "-" * width + "+")
+
+
+
+
+
